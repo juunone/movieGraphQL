6cbe1086e2802c4bd0ad0aa93715a64b6b0bb2e4
one <juunone@uneedcomms.com>
2018-07-12 18:43:33 +0900

    test

--- graphQL/db.js
+++ graphQL/db.js
@@ -1,49 +1,16 @@
-let movies = [
-    {
-        id:0,
-        name:"star wars",
-        score:10
-    },
-    {
-        id:1,
-        name:"avengers", 
-        score:5
-    },
-    {
-        id:2,
-        name:"batman",
-        score:3
-    },
-    {
-        id:3,
-        name:"superman",
-        score:8
+import fetch from 'node-fetch';
+const API_URL = "https://yts.am/api/v2/list_movies.json?" 
+
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if(limit > 0){
+        REQUEST_URL += `limit=${limit}`;
     }
-];
-
-export const getMovies = id => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-};
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if(movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    }else{
-        return false;
+    if(rating > 0){
+        REQUEST_URL += `&minimum_rating=${rating}`;
     }
+    return fetch(REQUEST_URL)
+        .then(res => res.json())
+        .then(json => json.data.movies);
 };
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-}
+    
--- graphQL/resolvers.js
+++ graphQL/resolvers.js
@@ -1,14 +1,9 @@
-import { getMovies, getById, addMovie, deleteMovie } from './db.js'
+import { getMovies } from './db.js'
 
 const resolvers = {
     Query : {
-        movies:() => getMovies(),
-        movie:(_, { id }) => getById(id)
-    },
-    Mutation:{
-        addMovie:(_, {name,score}) => addMovie(name, score),
-        deleteMovie: (_, { id }) => deleteMovie(id)
+        movies:(_, {rating,limit}) => getMovies(limit,rating)
     }
-}
+} 
 
 export default resolvers;
